ff439ab150e07d32031f2e31314db3af94524faf
piyushk6626 <kulkarnipiyush462@gmail.com>
2025-02-14 22:27:03 +0530

    workes little bit

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, jsonify
 from neo4j import GraphDatabase
 import os
-
+from dotenv import load_dotenv
 app = Flask(__name__)
+load_dotenv()
 
 # Set your Neo4j connection details (use environment variables or hardcode for testing)
-NEO4J_URI = os.getenv('NEO4J_URI', 'bolt://localhost:7687')
-NEO4J_USER = os.getenv('NEO4J_USER', 'neo4j')
-NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD', 'password')
+NEO4J_URI = os.getenv('NEO4J_URI')
+NEO4J_USER = os.getenv('NEO4J_USERNAME')
+NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
 
 driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
