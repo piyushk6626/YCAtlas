ff6473572d8dbbf7efda3f01699d71f9a46077e8
piyushk6626 <kulkarnipiyush462@gmail.com>
2025-02-15 18:18:56 +0530

    Added to runner function to __init__.py

--- ScrapingScripts/__init__.py
+++ ScrapingScripts/__init__.py
@@ -2,12 +2,13 @@
 
 from .scrape import scrape_page, process_csv
 from .links import setup_driver, Number_of_Loaded_Product, scroll_page, scrape_links
-
+from .runner import run_scraping_pipeline
 __all__ = [
     "scrape_page",
     "process_csv",
     "setup_driver",
     "Number_of_Loaded_Product",
     "scroll_page",
-    "scrape_links"
+    "scrape_links",
+    "run_scraping_pipeline"
 ] 
