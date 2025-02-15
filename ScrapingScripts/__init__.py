b34cdbf380c0c4b6c9039753485ccbdb1650d2fd
piyushk6626 <kulkarnipiyush462@gmail.com>
2025-02-15 18:04:38 +0530

    Made Code Modular

---
+++ ScrapingScripts/__init__.py
@@ -0,0 +1,13 @@
+# Make functions in the submodules available when importing the package
+
+from .scrape import scrape_page, process_csv
+from .links import setup_driver, Number_of_Loaded_Product, scroll_page, scrape_links
+
+__all__ = [
+    "scrape_page",
+    "process_csv",
+    "setup_driver",
+    "Number_of_Loaded_Product",
+    "scroll_page",
+    "scrape_links"
+] 
