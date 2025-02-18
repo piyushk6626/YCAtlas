d242ed01ab5adf817aa082bdf9b073089fc8bf8a
piyushk6626 <kulkarnipiyush462@gmail.com>
2025-02-18 14:49:24 +0530

    added resopnse

--- ScrapingScripts/CompaneyWebsite.py
+++ ScrapingScripts/CompaneyWebsite.py
@@ -1,8 +1,26 @@
 # Install with pip install firecrawl-py
 from firecrawl import FirecrawlApp
+from dotenv import load_dotenv
+import os
 
-app = FirecrawlApp(api_key='fc-8453af5805a8444ea032e9367d6f0bd5')
+load_dotenv()
+api_key = os.getenv('FIRECRAWL_API_KEY')
+
+app = FirecrawlApp(api_key=api_key)
 
 response = app.scrape_url(url='https://docs.mendable.ai', params={
 	'formats': [ 'markdown' ],
 })
+
+response={
+    "success": True,
+    "data": {
+      "markdown": "# Markdown Content",
+      "metadata": {
+        "title": "Mendable | AI for CX and Sales",
+        "description": "AI for CX and Sales",
+        "language": None,
+        "sourceURL": "https://www.mendable.ai/"
+      }
+    }
+  }
